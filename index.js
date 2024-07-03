60f0f769cd83146e870f95321605b453d4592ab2
Kallyne Layane <hellookallyne@gmail.com>
2024-07-03 10:26:04 -0300

    Initialize necessary variables

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const colorInput = document.querySelector('#color')
 const randomColorButton = document.querySelector('#random-color-button')
 const boardGuidesButton = document.querySelector('#board-guides-button')
 const clearBoardButton = document.querySelector('#clear-board-button')
+
+let mouseIsDown = false
+let currentColor = '#000000'
+let randomColorModeIsActived = false
+let boardGuidesIsActived = false
