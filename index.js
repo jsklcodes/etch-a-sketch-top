c0f72369e0990518ca64d491727143b1376a3abd
Kallyne Layane <hellookallyne@gmail.com>
2024-07-03 10:24:56 -0300

    Select DOM necessary elements

---
+++ index.js
@@ -0,0 +1,6 @@
+const boardDiv = document.querySelector('#board')
+const boardSizeButton = document.querySelector('#board-size-button')
+const colorInput = document.querySelector('#color')
+const randomColorButton = document.querySelector('#random-color-button')
+const boardGuidesButton = document.querySelector('#board-guides-button')
+const clearBoardButton = document.querySelector('#clear-board-button')
